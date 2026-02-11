f2ba614aba6c0b54b10384d3b8b093db87bd2fc1
dnyanucoder8830 <dnyaneshwaryeware493@gmail.com>
2026-02-11 11:49:42 +0530

    Practice Example 11/2/2026

---
+++ src/src/Exampel5.java
@@ -0,0 +1,15 @@
+public class Exampel5 {
+    public static void main(String[] args) {
+    Car max= (a,b)-> {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    };
+        System.out.println(max.m1(12,23));//it will print maximum number
+    };
+
+
+
+}
